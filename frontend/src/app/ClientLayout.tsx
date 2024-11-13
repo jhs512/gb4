6b7f13232cdfd6f8cb59b40f85b7af95c29b1381
jhs512 <jangka512@gmail.com>
2024-11-14 02:51:37 +0900

    work

--- frontend/src/app/ClientLayout.tsx
+++ frontend/src/app/ClientLayout.tsx
@@ -2,17 +2,52 @@
 
 import Link from "next/link";
 
+import client from "@/lib/openapi_fetch";
+import { useLoginMember } from "@/stores/member";
+import { useEffect } from "react";
+
 export default function ClientLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { setLoginMember, isLogin, loginMember, removeLoginMember } =
+    useLoginMember();
+
+  useEffect(() => {
+    client.GET("/api/v1/members/me").then(({ data }) => {
+      if (data) {
+        setLoginMember(data.data);
+      }
+    });
+  }, []);
+
+  const logout = () => {
+    client.POST("/api/v1/members/logout").then(({ error }) => {
+      if (error) {
+        alert(error.msg);
+      } else {
+        removeLoginMember();
+      }
+    });
+  };
+
   return (
     <>
       <header className="flex gap-2">
         <Link href="/">홈</Link>
         <Link href="/p/list">글 목록</Link>
-        <Link href="/member/login">로그인</Link>
+        {isLogin ? (
+          <>
+            <Link href={`/member/me`}>{loginMember.name}님 정보</Link>
+            <button onClick={logout}>로그아웃</button>
+          </>
+        ) : (
+          <>
+            <Link href="/member/login">로그인</Link>
+            <Link href="/member/join">회원가입</Link>
+          </>
+        )}
       </header>
       <main className="flex-1">{children}</main>
       <footer>- GB4 -</footer>
