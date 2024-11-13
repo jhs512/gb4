a439e0e7d086bd04d7c3823317e77a0b749d1c2f
jhs512 <jangka512@gmail.com>
2024-11-14 00:34:52 +0900

    work

--- frontend/src/app/p/[id]/ClientPage.tsx
+++ frontend/src/app/p/[id]/ClientPage.tsx
@@ -1,33 +1,19 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import client from "@/lib/openapi_fetch";
 import { components } from "@/lib/backend/apiV1/schema";
 
-export default function ClientPage({ id }: { id: number }) {
-  const [post, setPost] = useState<
-    components["schemas"]["PostDto"] | undefined
-  >(undefined);
-
-  useEffect(() => {
-    client
-      .GET("/api/v1/posts/{id}", {
-        params: {
-          path: {
-            id,
-          },
-        },
-      })
-      .then((res) => {
-        res.data && setPost(res.data);
-      });
-  }, []);
-
+export default function ClientPage({
+  id,
+  post,
+}: {
+  id: number;
+  post: components["schemas"]["PostDto"];
+}) {
   return (
     <div>
-      <h1>{post?.title}</h1>
+      <h1>{post.title}</h1>
       <p>
-        {post?.id} : {post?.body}
+        {post.id} : {post.body}
       </p>
     </div>
   );
--- frontend/src/app/p/[id]/page.tsx
+++ frontend/src/app/p/[id]/page.tsx
@@ -1,7 +1,29 @@
+import client from "@/lib/openapi_fetch";
 import ClientPage from "./ClientPage";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "",
+  description: "",
+};
 
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = await params;
 
-  return <ClientPage id={parseInt(id)} />;
+  const { data, error } = await client.GET("/api/v1/posts/{id}", {
+    params: {
+      path: {
+        id: parseInt(id),
+      },
+    },
+  });
+
+  if (error) {
+    return <div>Error: {error.msg}</div>;
+  }
+
+  metadata.title = data.title;
+  metadata.description = data.body;
+
+  return <ClientPage id={parseInt(id)} post={data} />;
 }
